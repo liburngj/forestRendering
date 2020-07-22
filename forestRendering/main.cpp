c30638c41d80fa5e8caa629f8a3fb496c3b00cbe
Liburn Gjonbalaj <liburn.gjonbalaj@byerley.ch>
2020-07-22 11:10:25 +0200

    Initial Commit

---
+++ forestRendering/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  forestRendering
+//
+//  Created by Liburn Gjonbalaj on 22.07.20.
+//  Copyright © 2020 Liburn Gjonbalaj. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
